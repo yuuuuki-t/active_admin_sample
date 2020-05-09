3be7b80fc23e82dd342ee7130abeddf6ee09ee77
yuki.t <cxntp@MacBook-Pro.local>
2020-05-09 11:55:39 -0700

    active_admin_sample

---
+++ app/admin/users.rb
@@ -0,0 +1,5 @@
+ActiveAdmin.register User do
+
+permit_params :name, :age, :address
+  
+end
---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module ActiveAdminSample0507
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+    config.i18n.default_locale = :ja
+  end
+end
